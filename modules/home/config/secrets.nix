36d6dce81726c0f2731c5bead0d9e2ab05f0cdeb
dustinm <dustinmmiao@gmail.com>
2026-05-16 09:44:17 -0700

    Added openai, anthropic API keys secret fields

--- modules/home/config/secrets.nix
+++ modules/home/config/secrets.nix
@@ -6,12 +6,23 @@
     defaultSopsFormat = "yaml";
     age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
 
-    secrets."git/email" = { };
+    secrets = {
+      "git/email" = { };
+      "tokens/openai" = { };
+      "tokens/anthropic" = { };
+    };
 
-    templates."git-user-include".content = ''
-      [user]
-        email = ${config.sops.placeholder."git/email"}
-    '';
+    templates = {
+      "git-user-include".content = ''
+        [user]
+          email = ${config.sops.placeholder."git/email"}
+      '';
+
+      "api-tokens.env".content = ''
+        export OPENAI_API_KEY="${config.sops.placeholder."tokens/openai"}"
+        export ANTHROPIC_API_KEY="${config.sops.placeholder."tokens/anthropic"}"
+      '';
+    };
   };
 
   home.packages = with pkgs; [ sops ssh-to-age age ];
