e1f9282918f0e66129cf0a72b7ba0803c16500e6
dustinm <dustinmmiao@gmail.com>
2026-05-17 14:04:04 -0700

    Edited key bindings for copy-paste in vscode

--- modules/home/config/vscode.nix
+++ modules/home/config/vscode.nix
@@ -38,9 +38,59 @@
         "extensions.autoUpdate" = false;
         "extensions.autoCheckUpdates" = false;
         "vim.useSystemClipboard" = true;
+        "vim.handleKeys" = {
+          "<C-c>" = false;
+          "<C-v>" = false;
+          "<C-a>" = false;
+          "<C-x>" = false;
+        };
         "workbench.colorTheme" = "Sonokai Shusia";
         "workbench.iconTheme" = "vscode-icons";
       };
+
+      keybindings = [
+        {
+          key = "cmd+c";
+          command = "editor.action.clipboardCopyAction";
+        }
+        {
+          key = "cmd+v";
+          command = "editor.action.clipboardPasteAction";
+        }
+        {
+          key = "cmd+c";
+          command = "workbench.action.terminal.copySelection";
+          when = "terminalFocus";
+        }
+        {
+          key = "cmd+v";
+          command = "workbench.action.terminal.paste";
+          when = "terminalFocus";
+        }
+        {
+          key = "ctrl+c";
+          command = "-editor.action.clipboardCopyAction";
+        }
+        {
+          key = "ctrl+v";
+          command = "-editor.action.clipboardPasteAction";
+        }
+        {
+          key = "ctrl+shift+c";
+          command = "-workbench.action.terminal.copySelection";
+          when = "terminalFocus";
+        }
+        {
+          key = "ctrl+shift+v";
+          command = "-workbench.action.terminal.paste";
+          when = "terminalFocus";
+        }
+        {
+          key = "shift+insert";
+          command = "-workbench.action.terminal.pasteSelection";
+          when = "terminalFocus";
+        }
+      ];
     };
   };
 }
