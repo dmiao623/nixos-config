9cfe59cb5433cbc9ae9378745c7e014271036a92
dustinm <dustinmmiao@gmail.com>
2026-05-23 21:40:49 -0700

    Added keyboard-specific kanata configurations for external DasKeyboard

--- modules/home/config/kanata.nix
+++ modules/home/config/kanata.nix
@@ -8,9 +8,11 @@
 {
   home.packages = [ pkgs.kanata ];
 
-  xdg.configFile."kanata/kanata.kbd".text = ''
+  # ThinkPad internal keyboard
+  xdg.configFile."kanata/thinkpad.kbd".text = ''
     (defcfg
       process-unmapped-keys yes
+      linux-dev-names-include ("AT Translated Set 2 keyboard")
     )
     (defsrc
       caps bspc rsft lmet lalt
@@ -26,13 +28,46 @@
     )
   '';
 
-  systemd.user.services.kanata = {
+  xdg.configFile."kanata/external.kbd".text = ''
+    (defcfg
+      process-unmapped-keys yes
+      linux-dev-names-include ("DasKeyboard")
+    )
+    (defsrc
+      caps bspc rsft
+    )
+    (deflayer default
+      bspc caps esc
+    )
+    (defoverrides
+      (lmet c) (lctl c)
+      (lmet v) (lctl v)
+      (lmet a) (lctl a)
+      (lmet z) (lctl z)
+    )
+  '';
+
+  systemd.user.services.kanata-thinkpad = {
     Unit = {
-      Description = "Kanata keyboard remapper";
+      Description = "Kanata keyboard remapper (ThinkPad)";
     };
     Service = {
       Environment = "PATH=${pkgs.kanata}/bin";
-      ExecStart = "${pkgs.kanata}/bin/kanata --cfg %h/.config/kanata/kanata.kbd";
+      ExecStart = "${pkgs.kanata}/bin/kanata --cfg %h/.config/kanata/thinkpad.kbd";
+      Restart = "always";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.services.kanata-external = {
+    Unit = {
+      Description = "Kanata keyboard remapper (external)";
+    };
+    Service = {
+      Environment = "PATH=${pkgs.kanata}/bin";
+      ExecStart = "${pkgs.kanata}/bin/kanata --cfg %h/.config/kanata/external.kbd";
       Restart = "always";
     };
     Install = {
